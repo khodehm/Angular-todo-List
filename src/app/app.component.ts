f0b4d719f610c6772fe0f5f2e016ac3a26c975e5
khodehm <aliteza861@gmail.com>
2024-02-19 01:23:03 +0330

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { item } from './model';
+import { NgFor } from '@angular/common';
+let _lastId = 0;
+function generatedId(): number {
+  return (_lastId += 1);
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.output.css',
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Todo';
+  filter: 'all' | 'active ' | 'done' = 'all';
+  container = 'container';
+  tasks = [
+    {
+      id: 1,
+      description: 'task 1',
+      done: true,
+    },
+    {
+      id: 2,
+      description: 'task 2',
+      done: false,
+    },
+    {
+      id: 3,
+      description: 'task 3',
+      done: true,
+    },
+    {
+      id: 4,
+      description: 'task 4',
+      done: false,
+    },
+  ];
+  get the_item() {
+    if (this.filter === 'all') {
+      return this.tasks;
+    }
+    return this.tasks.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
+  addItems(description: string) {
+    this.tasks.unshift({
+      id: generatedId(),
+      description,
+      done: false,
+    });
+  }
 }
---
+++ src/app/model.ts
@@ -0,0 +1,5 @@
+export interface item {
+  id: number;
+  description: string;
+  done: boolean;
+}
