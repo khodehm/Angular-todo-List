51a165027b7313d9313721e426160ca9b8ffe8a1
khodehm <aliteza861@gmail.com>
2024-02-26 22:41:38 +0330

    2024/2/26

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TodoComponent } from './todo/todo.component';
 import { CalenderMainPageComponent } from './calender-main-page/calender-main-page.component';
+import { notFound } from './404/404.component';
+import { AppComponent } from './app.component';
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'calender', component: CalenderMainPageComponent },
   {
     path: 'todo',
     component: TodoComponent,
   },
-  { path: '*', redirectTo: 'home' },
-  { path: 'calender', component: CalenderMainPageComponent },
+  { path: '', redirectTo: 'notFound', pathMatch: 'full' },
+
+  { path: '*', component: notFound },
+  { path: '**', redirectTo: 'notFound' },
 ];
